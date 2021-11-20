c5f3325beced7ccd2c65ce784ee20d9734190a89
Kijoon <igreen0485@gmail.com>
2021-11-21 07:42:45 +0900

    modified extractWords function

--- src/assets/words.js
+++ src/assets/words.js
@@ -1,12 +1,9 @@
 const extractWords = (arr)=>{
-    if(!arr) return [{text:"wait", value:1}]
+    if(!arr) return [{text:"wait", value:0}]
     const counts = {}
-    console.log("arr",arr)
-    console.log(arr.map(c=> c.text))
-    const raw = arr.map(comment =>{
-        return comment.text.split(/(\s+)/)
+    arr.map(comment =>{
+        comment.text.split(/(\s+)/).forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
     })
-    raw.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
     console.log("COUNTS",counts)
     return Object.keys(counts).map(count =>{
         return {text:count, value:counts[count]}
